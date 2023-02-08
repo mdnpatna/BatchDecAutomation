ea694e375332acf09bed9880e10438adee47481c
mdnpatna <mdnpatna@mdnpatna>
2023-02-08 12:34:25 +0530

    Commit 2

--- com.crm.actitime/src/test/java/n_BasicTextScript/b07_02_05_ToSendMailAndToVerify.java
+++ com.crm.actitime/src/test/java/n_BasicTextScript/b07_02_05_ToSendMailAndToVerify.java
@@ -9,6 +9,7 @@ public class b07_02_05_ToSendMailAndToVerify extends a07_02_04_BaseClass{
 	@Test
 	public void sendMailAndVerify() {
 		System.out.println("The mail has been sent and verified");
+		System.out.println("Hello");
 	}
 	
 
